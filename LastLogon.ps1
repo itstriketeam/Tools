70f05c4db21cb204525687858a8dd4c6f01367ed
itstriketeam <75801733+itstriketeam@users.noreply.github.com>
2020-12-14 10:43:42 -0500

    Add files via upload
    The lastLogon attribute is not replicated between domain controllers. Therefore, in order to determine when a user or computer last logged onto a domain you must iterate through all domain controllers and find the most recent entry. This script does that for you.

---
+++ LastLogon.ps1
@@ -0,0 +1,24 @@
+﻿Import-Module ActiveDirectory
+
+function Get-ADComputerLastLogon([string]$computerName)
+{
+  $dcs = Get-ADDomainController -Filter {Name -like "*"}
+  $time = 0
+  foreach($dc in $dcs)
+  { 
+    $hostname = $dc.HostName
+    $computer = Get-ADComputer $computerName | Get-ADObject -Server $hostname -Properties lastLogon 
+    if($computer.LastLogon -gt $time) 
+    {
+      $time = $computer.LastLogon
+    }
+  }
+  $dt = [DateTime]::FromFileTime($time)
+  Write-Host $computername "last logged on at:" $dt }
+
+if ($args[0] -eq $null){
+write-host "Must specify computer or user account name."
+ }
+else{
+Get-ADComputerLastLogon -ComputerName $args[0]
+}
